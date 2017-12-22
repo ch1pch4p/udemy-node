ae8308d989661c72b91b35cea8230f9ddab8784c
James Kingdon <kingdon909@gmail.com>
2017-12-22 03:02:47 -0500

    Simplified Input With Yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,21 +1,24 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('James'));
-var filteredArray = _.uniq([1,1,2,3]);
-console.log(filteredArray)
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs', argv);
 
-// console.log('Result:', notes.add(2,3));
-
-// var user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err) {
-//     if(err) {
-//         console.log('Unable to write to file');
-//     }
-// });
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read'){
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not recognized');
+}
