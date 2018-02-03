749aa8db4c6613a1365452748c0af3b27f02be59
James Kingdon <kingdon909@gmail.com>
2018-02-03 18:59:16 -0500

    Completed Using an Assertion Library

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,29 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
 
-    if(res !== 44) {
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
+    expect(res).toBe(44).toBeA('number');
+    // if(res !== 44) {
+    //     throw new Error(`Expected 44, but got ${res}.`);
+    // }
 });
 
 it('should add square a number', () => {
     var res = utils.square(5);
 
-    if(res !== 25) {
-        throw new Error(`Expected 25, but got ${res}.`);
-    }
+    expect(res).toBe(25).toBeA('number');
+});
+
+it('should verify first and last names are set', () => {
+   var user = {};
+   user.age = 18;
+   user.location = 'Dallas';
+   var result = utils.setName(user, "Jimmy Loggins");
+   expect(result).toInclude({
+       firstName: "Jimmy",
+       lastName: "Loggins"
+   })
 });
